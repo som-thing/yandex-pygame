c1ba31457f551c3d482ab60ba98b1807d8563304
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2022-12-22 18:11:51 +0600

    squares commit

--- Pygame 0.0/main.py
+++ Pygame 0.0/main.py
@@ -10,6 +10,8 @@ class Board:
         self.left = 10
         self.top = 10
         self.cell_size = 30
+        self.colors = [[0 for j in range(height)] for i in range(width)]
+        self.flag = True
 
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -17,15 +19,30 @@ class Board:
         self.cell_size = cell_size
 
     def render(self, screen):
+        colors = [pygame.Color('black'), pygame.Color('grey')]
         for y in range(self.height):
             for x in range(self.width):
-                pygame.draw.rect(screen, (255, 255, 255), (x * self.cell_size + self.left,
+                pygame.draw.rect(screen, colors[self.board[y][x]], (x * self.cell_size + self.left,
                                                            y * self.cell_size + self.top,
-                                                           self.cell_size, self.cell_size), 1)
+                                                           self.cell_size, self.cell_size))
+                pygame.draw.rect(screen, pygame.Color('white'), (x * self.cell_size + self.left,
+                                                                 y * self.cell_size + self.top,
+                                                                 self.cell_size,
+                                                                 self.cell_size), 1)
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
-        self.on_click(cell)
+        if self.flag:
+            self.on_click(cell)
+            self.first_cell = cell
+            self.flag = not self.flag
+        else:
+            if (abs(self.first_cell[0] - cell[0]) == 1 and self.first_cell[1] == cell[1]) \
+                    or (abs(self.first_cell[1] - cell[1]) == 1 and self.first_cell[0] == cell[0]):
+                self.on_click(cell)
+            else:
+                self.begin(self.first_cell)
+            self.flag = not self.flag
 
     def get_cell(self, mouse_pos):
         for y in range(self.height):
@@ -36,7 +53,10 @@ class Board:
         return None
 
     def on_click(self, cell):
-        pass
+        self.board[cell[1]][cell[0]] = 1
+
+    def begin(self, cell):
+        self.board[cell[1]][cell[0]] = 0
 
 
 def main():
@@ -52,6 +72,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                board.get_click(event.pos)
         screen.fill((0, 0, 0))
         board.render(screen)
         pygame.display.flip()
