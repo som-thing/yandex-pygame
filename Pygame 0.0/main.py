2dc1e154cd759dd8c4a6569fc378074ac2e4fc39
som-thing <81899399+som-thing@users.noreply.github.com>
2022-12-24 11:38:44 +0600

    Ribbons, errorfix + created TODO.txt
    Ribbons now appear.

--- Pygame 0.0/main.py
+++ Pygame 0.0/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 class Board:
@@ -12,6 +13,8 @@ class Board:
         self.cell_size = 30
         self.colors = [[0 for j in range(height)] for i in range(width)]
         self.flag = True
+        self.ribbons = {}  # здесь будут хранится соединения
+        self.ribbon_color = (random.choice(range(1, 256)), random.choice(range(1, 256)), random.choice(range(1, 256)))
 
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -23,28 +26,52 @@ class Board:
         for y in range(self.height):
             for x in range(self.width):
                 pygame.draw.rect(screen, colors[self.board[y][x]], (x * self.cell_size + self.left,
-                                                           y * self.cell_size + self.top,
-                                                           self.cell_size, self.cell_size))
+                                                                    y * self.cell_size + self.top,
+                                                                    self.cell_size, self.cell_size))
                 pygame.draw.rect(screen, pygame.Color('white'), (x * self.cell_size + self.left,
                                                                  y * self.cell_size + self.top,
                                                                  self.cell_size,
                                                                  self.cell_size), 1)
+                if (x, y) in self.ribbons.keys():  # прорисовка ленточек
+                    if self.ribbons[(x, y)] == "left":
+                        pygame.draw.rect(screen, self.ribbon_color, ((x - 1) * self.cell_size + self.left + 7,
+                                                                     y * self.cell_size + self.top + 5,
+                                                                     2 * self.cell_size - 14,
+                                                                     self.cell_size - 10))
+                    if self.ribbons[(x, y)] == "up":
+                        pygame.draw.rect(screen, self.ribbon_color, (x * self.cell_size + self.left + 5,
+                                                                     (y - 1) * self.cell_size + self.top + 7,
+                                                                     self.cell_size - 10,
+                                                                     2 * self.cell_size - 14))
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
-        if self.flag:
-            self.on_click(cell)
-            self.first_cell = cell
-            self.flag = not self.flag
-        else:
-            if self.first_cell == cell:
+        if cell:  # проверка, нажали ли на клетку или на пустое пространство
+            if self.board[cell[1]][cell[0]] == 1:
                 pass
-            elif (abs(self.first_cell[0] - cell[0]) == 1 and self.first_cell[1] == cell[1]) \
-                    or (abs(self.first_cell[1] - cell[1]) == 1 and self.first_cell[0] == cell[0]):
+            elif self.flag:
                 self.on_click(cell)
+                self.first_cell = cell
+                self.flag = not self.flag
             else:
-                self.begin(self.first_cell)
-            self.flag = not self.flag
+                if (abs(self.first_cell[0] - cell[0]) == 1 and self.first_cell[1] == cell[1]) \
+                        or (abs(self.first_cell[1] - cell[1]) == 1 and self.first_cell[0] == cell[0]):
+                    self.on_click(cell)
+                    if self.first_cell[0] - cell[0] == 1:  # проверяем как находятся относительно друг друга клетки
+                        self.ribbons[self.first_cell] = "left"
+                        self.ribbons[cell] = "right"
+                    elif self.first_cell[0] - cell[0] == -1:
+                        self.ribbons[self.first_cell] = "right"
+                        self.ribbons[cell] = "left"
+                    elif self.first_cell[1] - cell[1] == 1:
+                        self.ribbons[self.first_cell] = "up"
+                        self.ribbons[cell] = "down"
+                    else:
+                        self.ribbons[self.first_cell] = "down"
+                        self.ribbons[cell] = "up"
+                else:
+                    self.begin(self.first_cell)
+                self.flag = not self.flag
 
     def get_cell(self, mouse_pos):
         for y in range(self.height):
